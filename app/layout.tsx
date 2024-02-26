3084b999796772d9f3a532f4d5678476d8b72941
Damian Bartosiak <bartosiak.contact@gmail.com>
2024-02-26 14:03:03 +0100

    Added header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,28 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bartosiak Portfolio",
-  description: "Full-stack website developer",
+    title: "Bartosiak Portfolio",
+    description: "Full-stack website developer",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} bg-grey-50 text-gray-950 relative`}>
-
-        {children}
-        </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${inter.className} bg-gray-400 text-gray-950 relative`}
+            >
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-
 export default function Home() {
-  return (
-    <main className="">
-      <h1>Hello</h1>
-    </main>
-  );
+    return <main className=""></main>;
 }
